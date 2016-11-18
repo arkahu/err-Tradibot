4d0fbf43889917b3f4fa8982ad6288b8904bc779
Arttu H <arttuhut@gmail.com>
2016-11-18 22:44:29 +0200

    Version 0.3
    Added config file. New command for vocabulary initialization. !command
    no longer collected. Vocabulary linking in both ways. Error scorrected.

---
+++ tradibot_conf.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 22:30:43 2016
+
+Configuration variables for Errbot plugin Tradibot
+"""
+
+
+chatroom = '#general'
+
+vocabularyfile = 'vocabulary.txt'
+
+
